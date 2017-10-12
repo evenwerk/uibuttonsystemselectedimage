1323571e27961a14d12a2378560415905ac56758
Tim Even <evenwerk@gmail.com>
2017-10-12 12:13:33 +0200

    Initial Commit

---
+++ System Selected Image/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  System Selected Image
+//
+//  Created by Tim Even on 12/10/2017.
+//  Copyright © 2017 evenwerk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
